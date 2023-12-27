3e4c9652ff7214a4c5692bc67577da1f6b3378d2
Ankit Arora <05.ankitarora@gmail.com>
2023-12-27 19:40:24 +0530

    Create serviceWorker.js

---
+++ serviceWorker.js
@@ -0,0 +1,21 @@
+const staticDevCoffee = "bmt-daily-cache-v1"
+const assets = [
+  "/",
+  "/index.html",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
