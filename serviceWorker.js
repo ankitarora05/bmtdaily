a569610ffc5ee4150ef89be918a2a5dd0a7bff52
Ankit Arora <05.ankitarora@gmail.com>
2023-12-27 21:25:00 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,21 +1,21 @@
-const bmtDailyWebCache = "bmt-daily-cache-v2"
+const bmtDailyPWACache = "bmt-daily-site-v2";
 const assets = [
   "/",
   "/index.html",
-]
+];
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(bmtDailyWebCache).then(cache => {
-      cache.addAll(assets)
+    caches.open(bmtDailyPWACache).then(cache => {
+      cache.addAll(assets);
     })
-  )
-})
+  );
+});
 
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
     caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
+      return res || fetch(fetchEvent.request);
     })
-  )
-})
+  );
+});
