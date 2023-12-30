69ebe1426c328d20d710fddc2e3e871a9163da29
Ankit Arora <05.ankitarora@gmail.com>
2023-12-30 17:31:17 +0530

    Update and rename pwa-worker-file.js to pwa-worker-v1.js

---
+++ pwa-worker-v1.js
@@ -0,0 +1,35 @@
+const bmtDailyPWACache = "bmt-daily-site-v6";
+const assets = [
+  "/",
+  "/index.html",
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(bmtDailyPWACache).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  async function returnCachedResource() {
+    // Open the app's cache.
+    const cache = await caches.open(bmtDailyPWACache);
+    // Find the response that was pre-cached during the `install` event.
+    const cachedResponse = await cache.match(event.request.url);
+
+    if (cachedResponse) {
+      // Return the resource.
+      return cachedResponse;
+    } else {
+      // The resource wasn't found in the cache, so fetch it from the network.
+      const fetchResponse = await fetch(event.request.url);
+      // Put the response in cache.
+      cache.put(event.request.url, fetchResponse.clone());
+      // And return the response.
+      return fetchResponse.
+    }
+  }
+  event.respondWith(returnCachedResource());
+});
