f3ff4e596234495a84ea46d5f463a448a6aaaa3e
Ankit Arora <05.ankitarora@gmail.com>
2023-12-30 17:18:34 +0530

    Update pwa-worker-file.js

--- pwa-worker-file.js
+++ pwa-worker-file.js
@@ -1,4 +1,4 @@
-const bmtDailyPWACache = "bmt-daily-site-v5";
+const bmtDailyPWACache = "bmt-daily-site-v6";
 const assets = [
   "/",
   "/index.html",
@@ -12,10 +12,23 @@ self.addEventListener("install", installEvent => {
   );
 });
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(bmtDailyPWACache).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    (async function () {
+      // Try the cache
+      const cachedResponse = await caches.match(event.request);
+      if (cachedResponse) return cachedResponse;
+
+      try {
+        // Fall back to network
+        return await fetch(event.request);
+      } catch (err) {
+        // If both fail, show a generic fallback:
+        return caches.match('/offline.html');
+        // However, in reality you'd have many different
+        // fallbacks, depending on URL & headers.
+        // Eg, a fallback silhouette image for avatars.
+      }
+    })(),
+  );
+});
