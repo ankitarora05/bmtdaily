1e08cf0db4b0ea7fe0f247fa1bfc47cf1aef595b
Ankit Arora <05.ankitarora@gmail.com>
2024-01-07 20:27:13 +0530

    Update pwa-worker-v7.js

--- pwa-worker-v7.js
+++ pwa-worker-v7.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE_NAME = 'static-cache-v15';
-const APP_CACHE_NAME = 'app-cache-v15';
+const STATIC_CACHE_NAME = 'static-cache-v16';
+const APP_CACHE_NAME = 'app-cache-v16';
 
 const CACHE_APP = [
     '/',
